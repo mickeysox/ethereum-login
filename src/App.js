5279d12ca9fe3184ef8bd2cb23c83b3a71d63863
Michael Reay-Beaton <michael@berry.love>
2022-01-18 19:51:30 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
+import Ceramic from '@ceramicnetwork/http-client'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+
+
+import { ThreeIdConnect, EthereumAuthProvider } from '@3id/connect'
+import { DID } from 'dids'
+import { IDX } from '@ceramicstudio/idx'
+
+const endpoint = "https://ceramic-clay.3boxlabs.com"
 
 function App() {
+
+
+  async function authenticate() {  
+    const ethereum = window.ethereum;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    const addresses = await ethereum.request({ method: 'eth_requestAccounts' })
+    const address = addresses[0] 
+
+    const threeIdConnect = new ThreeIdConnect()
+    const authProvider = new EthereumAuthProvider(window.ethereum, address)
+
+    await threeIdConnect.connect(authProvider)
+
+    const ceramic = new Ceramic(endpoint)
+    const did = new DID({
+      provider: threeIdConnect.getDidProvider(),
+      resolver: ThreeIdResolver.getResolver(ceramic)
+    })
+
+    await did.authenticate()
+    ceramic.setDID(did)
+    console.log(did.id)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={authenticate}>Read Profile</button>
     </div>
   );
 }
